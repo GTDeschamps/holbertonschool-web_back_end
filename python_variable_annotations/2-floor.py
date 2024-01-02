b84ab3ceb19925247c525422051f43406e436ad9
GTDeschamps <g.deschamps@hotmail.fr>
2024-01-02 13:44:07 +0100

    Correction of Project 1

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -3,7 +3,7 @@
     as argument and returns the floor of the float."""
 
 
-def floor(n: float) -> float:
+def floor(n: float) -> int:
     """Write a type-annotated function floor which takes a float n
     as argument and returns the floor of the float."""
 
--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -6,7 +6,7 @@ mxd_lst of integers and floats and returns their sum as a float."""
 import typing
 
 
-def sum_mixed_list(mxd_lst: typing.List[typing.Union[int, float]]) -> list:
+def sum_mixed_list(mxd_lst: typing.List[typing.Union[int, float]]) -> float:
     """Write a type-annotated function sum_mixed_list which takes a list
     mxd_lst of integers and floats and returns their sum as a float."""
 
