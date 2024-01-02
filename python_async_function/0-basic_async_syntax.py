7e0cd45aca7cafe31717effd6d76e6b5c2bdfe81
GTDeschamps <g.deschamps@hotmail.fr>
2024-01-02 15:34:25 +0100

    correction of T0

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -10,14 +10,9 @@ import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """_summary_
-    Args:
-        max_delay (int, optional): _description_. Defaults to 10.
-    Returns:
-        float: _description_
-    """
+    """_definition of wait_random"""
 
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
 
-    return(delay)
+    return (delay)
