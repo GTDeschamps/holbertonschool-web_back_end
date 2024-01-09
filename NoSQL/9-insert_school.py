30986dea794ad4954cf324998535ec6da6bb7275
GTDeschamps <g.deschamps@hotmail.fr>
2024-01-09 14:44:56 +0100

    update Task9

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -4,5 +4,7 @@ based on kwargs"""
 
 
 def insert_school(mongo_collection, **kwargs):
+    """ function insert_id"""
+    
     inserted_id = mongo_collection.insert_one(kwargs).inserted_id
     return inserted_id
