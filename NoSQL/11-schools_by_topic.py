a8e4e681b9babec01be107ffb9e450d4a8fb34b1
GTDeschamps <g.deschamps@hotmail.fr>
2024-01-09 15:16:22 +0100

    Task 11

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Write a Python function that returns the list of school
+having a specific topic"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """ function return the list with specific topics"""
+
+    schools = mongo_collection.find({"topics": {"$in": [topic]}})
+    return list(schools)
