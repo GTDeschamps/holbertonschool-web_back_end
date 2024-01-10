ab7c32b90278816edf4daf1c1d06a99dfa0f466d
GTDeschamps <g.deschamps@hotmail.fr>
2024-01-10 14:18:08 +0100

    Pycodestyle T2

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -10,24 +10,25 @@ from typing import List
 
 Server = __import__('1-simple_pagination').Server
 
+
 def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        assert isinstance(page, int) and page > 0
-        "Page number must be an integer greater than 0"
-        assert isinstance(page_size, int) and page_size > 0
-        "Page size must be an integer greater than 0"
+    assert isinstance(page, int) and page > 0
+    "Page number must be an integer greater than 0"
+    assert isinstance(page_size, int) and page_size > 0
+    "Page size must be an integer greater than 0"
 
-        data = []
-        with open('Popular_Baby_Names.csv', newline='') as csvfile:
-            reader = csv.reader(csvfile)
-            dataset = list(reader)
+    data = []
+    with open('Popular_Baby_Names.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        dataset = list(reader)
 
-            start_index, end_index = index_range(page, page_size)
-            if start_index >= len(dataset):
-                return []
-            elif end_index >= len(dataset):
-                return dataset[start_index:]
-            else:
-                return dataset[start_index:end_index + 1]
+        start_index, end_index = index_range(page, page_size)
+        if start_index >= len(dataset):
+            return []
+        elif end_index >= len(dataset):
+            return dataset[start_index:]
+        else:
+            return dataset[start_index:end_index + 1]
 
 
 def get_hyper(page=1, page_size=10):
