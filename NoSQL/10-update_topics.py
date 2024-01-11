9b4de71ac3d7698ad0f66a5ef52189f3d17bb70d
GTDeschamps <g.deschamps@hotmail.fr>
2024-01-11 22:48:23 +0100

    update Task 10 & 12

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -7,7 +7,4 @@ def update_topics(mongo_collection, name, topics):
     """ function Update topics to change documents"""
 
     mongo_collection.update_many
-    (
-        {'name': name},
-        {"$set": {'topics': topics}}
-    )
+    ({'name': name}, {"$set": {'topics': topics}})
--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+"""Write a Python script that provides some stats
+about Nginx logs stored in MongoDB"""
+
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+
+    client = MongoClient('localhost', 27017)
+    collection = client.logs.nginx
+
+    total_log = collection.count_documents({})
+
+    print("{} logs".format(total_log))
+    print("Methods:")
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = collection.count_documents({"method" : method})
+        print("\tmethod {}: {}".format(method, count))
+
+    statut = collection.count_documents({"method" : "GET", "path": "/status"})
+    print("{} status check".format(statut))
