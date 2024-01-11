b70f8cc2918416d38f4b80e0d7f0bed54b570854
GTDeschamps <g.deschamps@hotmail.fr>
2024-01-11 23:00:11 +0100

    update Task 1

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -4,7 +4,6 @@ with default value 1 and page_size with default value 10."""
 
 
 import csv
-import math
 from typing import List
 
 index_range = __import__('0-simple_helper_function').index_range
@@ -19,7 +18,8 @@ class Server:
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
+        """
+        Cached dataset
         """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
@@ -30,20 +30,12 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Page number must be an integer greater than 0
+        """
         assert isinstance(page, int) and page > 0
-        "Page number must be an integer greater than 0"
+
         assert isinstance(page_size, int) and page_size > 0
-        "Page size must be an integer greater than 0"
 
-        data = []
-        with open('Popular_Baby_Names.csv', newline='') as csvfile:
-            reader = csv.reader(csvfile)
-            dataset = list(reader)
-
-            start_index, end_index = index_range(page, page_size)
-            if start_index >= len(dataset):
-                return []
-            elif end_index >= len(dataset):
-                return dataset[start_index:]
-            else:
-                return dataset[start_index:end_index + 1]
+        start_index, end_index = index_range(page, page_size)
+        return self.dataset()[start_index:end_index]
