9dc324811b50f35e4df8196a09b91d540ade1aae
GTDeschamps <g.deschamps@hotmail.fr>
2024-01-17 10:18:26 +0100

    modify Task 10-11-12

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-	const arrayNew = [];
-	for (const index of array) {
-		arrayNew.push(appendString + index);
-	}
+  const arrayNew = [];
+  for (const index of array) {
+    arrayNew.push(appendString + index);
+  }
 
-	return arrayNew;
+  return arrayNew;
 }
--- ES6_basic/11-createEmployeesObject.js
+++ ES6_basic/11-createEmployeesObject.js
@@ -1,8 +1,7 @@
 export default function createEmployeesObject(departmentName, employees) {
+  const departmentObject = {
+    [departmentName]: [ ...employees ]
+    };
 
-	const departmentObject = {
-		[departmentName]: [ ...employees ]
-	  };
-
-	  return departmentObject;
-	};
+  return departmentObject;
+};
