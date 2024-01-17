6a9b9ab601602a9724bf1f3a8ffca9f5eaed8b0e
GTDeschamps <g.deschamps@hotmail.fr>
2024-01-17 11:14:03 +0100

    project ES6_classes: Task 3

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,26 @@
+class Currency {
+  constructor(code, name) {
+    this._code = this.validateString(code, 'code');
+    this._name = this.validateString(name, 'name');
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    this._code = this.validateString(value, 'code');
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = this.validateString(value, 'name');
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
