c64f6d96d0fd31e41491d55470bbe0bc3e5a0af5
GTDeschamps <g.deschamps@hotmail.fr>
2024-01-17 18:25:32 +0100

    Fix Task 6

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -2,26 +2,22 @@ import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
+    if (typeof floors !== 'number' || floors <= 0) {
+      throw new Error('floors must be a valid positive number.');
+    }
     super(sqft);
-    this._floors = this.validateNumber(floors, 'floors');
+    this._floors = floors;
   }
 
   get floors() {
     return this._floors;
   }
 
-  set floors(value) {
-    this._floors = this.validateNumber(value, 'floors');
+  set floors(floors) {
+    this._floors = floors;
   }
 
   evacuationWarningMessage() {
     return `Evacuate slowly the ${this._floors} floors.`;
-  }
-
-  validateNumber(value, attribute) {
-    if (typeof value !== 'number' || isNaN(value) || value <= 0) {
-      throw new Error(`${attribute} must be a valid positive number.`);
-    }
-    return value;
   }
 }
