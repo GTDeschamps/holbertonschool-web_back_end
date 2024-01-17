2b32131ba7b3d3926d356f414f60d7c9d7eeb10f
GTDeschamps <g.deschamps@hotmail.fr>
2024-01-17 18:06:10 +0100

    fix Task 3

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,25 +1,23 @@
 export default class Currency {
   constructor(code, name) {
-    this._code = this.validateString(code, 'code');
-    this._name = this.validateString(name, 'name');
-  }
-
-  get code() {
-    return this._code;
-  }
-
-  set code(value) {
-    this._code = this.validateString(value, 'code');
+    this._code = code;
+    this._name = name;
   }
 
   get name() {
     return this._name;
   }
 
-  set name(value) {
-    this._name = this.validateString(value, 'name');
+  set name(name) {
+    this._name = name;
+  }
+  get code() {
+    return this._code;
   }
 
+  set code(code) {
+    this._code = code;
+  }
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }
