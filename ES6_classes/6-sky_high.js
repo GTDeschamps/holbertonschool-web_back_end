4aee53b197546552ad01d372318201c5ef7ee31c
GTDeschamps <g.deschamps@hotmail.fr>
2024-01-17 14:50:04 +0100

    Project ES6_classes: Task 6

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,27 @@
+import Building from './5-building.js';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = this.validateNumber(floors, 'floors');
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(value) {
+    this._floors = this.validateNumber(value, 'floors');
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors.`;
+  }
+
+  validateNumber(value, attribute) {
+    if (typeof value !== 'number' || isNaN(value) || value <= 0) {
+      throw new Error(`${attribute} must be a valid positive number.`);
+    }
+    return value;
+  }
+}
