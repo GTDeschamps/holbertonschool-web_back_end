2d24600b59622a089812e759aa521272f1210179
GTDeschamps <g.deschamps@hotmail.fr>
2024-01-17 09:57:52 +0100

    modify Task 6 & 7

--- ES6_basic/7-getBudgetObject.js
+++ ES6_basic/7-getBudgetObject.js
@@ -1,3 +1,9 @@
 export default function getBudgetObject(income, gpd, capita) {
-  return { income, gpd, capita };
+  const budget = {
+    income,
+    gpd,
+    capita,
+  };
+
+  return budget;
 }
