ef8b3661d8f62e40759f841193ac0614e70d813c
GTDeschamps <g.deschamps@hotmail.fr>
2024-01-17 18:34:24 +0100

    fix task 7

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -1,7 +1,7 @@
 export default class Airport {
   constructor(name, code) {
     if (typeof name !== 'string') {
-      throw new TypeError(`name must be a string`)
+      throw new TypeError('name must be a string');
     }
     this._name = name;
     this._code = code;
