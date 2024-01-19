c016587d0addb5138def0dbae3233572aa64006e
GTDeschamps <g.deschamps@hotmail.fr>
2024-01-19 11:16:48 +0100

    modify Task 1

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -3,7 +3,7 @@ import ClassRoom from './0-classroom';
 export default function initializeRooms() {
   const room = [
     new ClassRoom(19),
-    new ClassRoom(28),
+    new ClassRoom(20),
     new ClassRoom(34),
   ];
 
