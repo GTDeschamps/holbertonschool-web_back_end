ecd3fd7113dd90b561402cbf4417cd926facef9b
GTDeschamps <g.deschamps@hotmail.fr>
2024-01-23 15:55:27 +0100

    Task 0 &1 Project 2: Promise

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,12 @@
+export default function getFullResponseFromAPI(success) {
+  return new Promise ((Resolve, reject) => {
+    if (success) {
+      const result = { status: 200, body: 'Success' };
+      Resolve(result);
+    } else {
+      const error= new Error('The fake API is not working currently');
+      reject(error);
+    }
+  });
+}
+
