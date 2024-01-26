cce801b53bb8095534c7378a90ffb6bd93afdfdf
GTDeschamps <g.deschamps@hotmail.fr>
2024-01-26 14:49:01 +0100

    correction Task 3

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,10 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default async function handleProfileSignup() {
-  const photopromise = uploadPhoto();
-  const userpromise = createUser();
-
-  Promise.all([photopromise, userpromise])
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
     .then((value) => {
       console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
     })
