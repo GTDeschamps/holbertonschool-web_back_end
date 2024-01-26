e27845dca9eba24dc292c4c83b1dfd0f6c6877ca
GTDeschamps <g.deschamps@hotmail.fr>
2024-01-26 14:42:26 +0100

    Task 2 correction

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,14 +1,11 @@
 export function handleResponseFromAPI(promise) {
-  return new promise((resolve, reject) => {
-    promise
+  return promise
     .then(() => {
-      console.log('Got a response from the API');
-      const result = { status: 200, body: 'success' };
-      resolve (result);
+      const response = { status: 200, body: 'success' };
+      return (response);
     })
-    .catch((error) => {
-      console.error('Got an error from the API:', error.message);
-      reject (new Error(''));
+    .catch(() => new Error())
+    .finally(() => {
+      console.log('Got a response from the API:');
     });
-  });
 }
