79a836e1fa304fdeec00b0bf0e2f0aa06f63afbf
GTDeschamps <g.deschamps@hotmail.fr>
2024-01-29 10:31:37 +0100

    Project ES6_data_manipulation: Task 0: array of objects

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function getListStudents() {
+  return [
+    { id: '1', firstName: 'Guillaume', Location: 'San Francisco' },
+    { id: '2', firstName: 'James', Location: 'Columbia' },
+    { id: '5', firstName: 'Serena', Location: 'San Francisco' },
+  ];
+}
