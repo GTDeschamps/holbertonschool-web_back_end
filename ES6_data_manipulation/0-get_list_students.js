873a96a4c069932edd9c428b59e9d6811ce20ad4
GTDeschamps <g.deschamps@hotmail.fr>
2024-01-29 11:31:19 +0100

    Update Task 0

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,13 @@
 export default function getListStudents() {
-  return [
+  const studentData = [
     { id: '1', firstName: 'Guillaume', location: 'San Francisco' },
     { id: '2', firstName: 'James', location: 'Columbia' },
     { id: '5', firstName: 'Serena', location: 'San Francisco' },
   ];
+  const students = studentData.map((student) => ({
+    id: student.id,
+    firstName: student.firstName,
+    location: student.location,
+  }));
+  return students;
 }
